9a9c49f3089f888d04ff68ebc3eb288b617993dc
huabbouchi <huabbouchi@yahoo.com>
2024-01-17 08:59:27 +0100

    update app

--- blog_outline_generator_app.py
+++ blog_outline_generator_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
 from langchain import PromptTemplate
 
 st.set_page_config(page_title="🦜🔗 Blog Outline Generator App")
